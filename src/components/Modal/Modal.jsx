efd95bbf81c054fc764127d574c94a2266597def
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-09-22 00:52:57 +0300

    Moved Modal to hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,41 +1,78 @@
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handlerEscape);
-  }
+const Modal = ({ onClose, largeImageURL, tags }) => {
+  useEffect(() => {
+    console.log('Компонент замаунтився в модалці');
+    window.addEventListener('keydown', handlerEscape);
+    return () => {
+      console.log('Компонент розмонтувався в модалці');
+      window.removeEventListener('keydown', handlerEscape);
+    };
+  }, []);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handlerEscape);
-  }
-
-  handlerEscape = e => {
+  const handlerEscape = e => {
     if (e.code === 'Escape') {
       //   console.log('e.code', e.code);
-      this.props.onClose();
+      onClose();
     }
   };
 
-  handleBackdropClick = event => {
+  const handleBackdropClick = event => {
     if (event.target === event.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    const { handleBackdropClick } = this;
-    const { largeImageURL, tags } = this.props;
-    // console.log(largeImageURL);
-    return createPortal(
-      <div className="Overlay" onClick={handleBackdropClick}>
-        <div className="Modal">
-          <img src={largeImageURL} alt={tags} />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <div className="Overlay" onClick={handleBackdropClick}>
+      <div className="Modal">
+        <img src={largeImageURL} alt={tags} />
+      </div>
+    </div>,
+    modalRoot
+  );
+};
+
+export default Modal;
+
+// export default class Modal extends Component {
+// componentDidMount() {
+//   console.log('Компонент замаунтився в модалці');
+//   window.addEventListener('keydown', this.handlerEscape);
+// }
+
+// componentWillUnmount() {
+//   console.log('Компонент розмонтувався в модалці');
+//   window.removeEventListener('keydown', this.handlerEscape);
+// }
+
+// handlerEscape = e => {
+//   if (e.code === 'Escape') {
+//     //   console.log('e.code', e.code);
+//     this.props.onClose();
+//   }
+// };
+
+// handleBackdropClick = event => {
+//   if (event.target === event.currentTarget) {
+//     this.props.onClose();
+//   }
+// };
+
+//   render() {
+//     const { handleBackdropClick } = this;
+//     const { largeImageURL, tags } = this.props;
+//     // console.log(largeImageURL);
+// return createPortal(
+//   <div className="Overlay" onClick={handleBackdropClick}>
+//     <div className="Modal">
+//       <img src={largeImageURL} alt={tags} />
+//     </div>
+//   </div>,
+//   modalRoot
+// );
+//   }
+// }
