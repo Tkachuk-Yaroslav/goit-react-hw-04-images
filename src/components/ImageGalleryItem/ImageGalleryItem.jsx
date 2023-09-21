4738fa6e3dedd54d42efb81d2a21eb68ef165497
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-09-22 01:06:02 +0300

    Moved ImageGalleryItem to hooks

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,40 +1,72 @@
 import Modal from 'components/Modal/Modal';
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import { useState } from 'react';
 
-export default class ImageGalleryItem extends Component {
-  state = {
-    showModal: false,
+const ImageGalleryItem = ({
+  image: { id, largeImageURL, webformatURL, tags },
+}) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(prev => !prev);
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
-  };
+  return (
+    <li key={id} className="ImageGalleryItem">
+      <img
+        className="ImageGalleryItem-image"
+        src={webformatURL}
+        alt={largeImageURL}
+        onClick={toggleModal}
+      />
 
-  render() {
-    const { showModal } = this.state;
-    const { image } = this.props;
-    const { id, largeImageURL, webformatURL, tags } = image;
-    const { toggleModal } = this;
-
-    return (
-      <li key={id} className="ImageGalleryItem">
-        <img
-          className="ImageGalleryItem-image"
-          src={webformatURL}
-          alt={largeImageURL}
-          onClick={toggleModal}
+      {showModal && (
+        <Modal
+          largeImageURL={largeImageURL}
+          tags={tags}
+          onClose={toggleModal}
         />
+      )}
+    </li>
+  );
+};
 
-        {showModal && (
-          <Modal
-            largeImageURL={largeImageURL}
-            tags={tags}
-            onClose={toggleModal}
-          />
-        )}
-      </li>
-    );
-  }
-}
+export default ImageGalleryItem;
+
+// export default class ImageGalleryItem extends Component {
+//   state = {
+//     showModal: false,
+//   };
+
+// toggleModal = () => {
+//   this.setState(({ showModal }) => ({
+//     showModal: !showModal,
+//   }));
+// };
+
+//   render() {
+//     const { showModal } = this.state;
+//     const { image } = this.props;
+//     const { id, largeImageURL, webformatURL, tags } = image;
+//     const { toggleModal } = this;
+
+//     return (
+//       <li key={id} className="ImageGalleryItem">
+//         <img
+//           className="ImageGalleryItem-image"
+//           src={webformatURL}
+//           alt={largeImageURL}
+//           onClick={toggleModal}
+//         />
+
+//         {showModal && (
+//           <Modal
+//             largeImageURL={largeImageURL}
+//             tags={tags}
+//             onClose={toggleModal}
+//           />
+//         )}
+//       </li>
+//     );
+//   }
+// }
